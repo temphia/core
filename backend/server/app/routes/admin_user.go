2ec65f0ee32be9415b2307b2df4702d7afc79503
bornjre <bornjre@gmail.com>
2022-05-17 02:48:09 +0545

    refactor

--- backend/server/app/routes/admin_user.go
+++ backend/server/app/routes/admin_user.go
@@ -66,3 +66,51 @@ func (r *R) UserProfileImage(ctx *gin.Context) {
 	}
 	apiutils.WriteBinary(ctx, bytes)
 }
+
+// perm placeholder stuff
+
+func (r *R) AddPerm(ctx request.Ctx) {
+
+}
+func (r *R) UpdatePerm(ctx request.Ctx) {
+
+}
+func (r *R) GetPerm(ctx request.Ctx) {
+
+}
+func (r *R) RemovePerm(ctx request.Ctx) {
+
+}
+func (r *R) AddRole(ctx request.Ctx) {
+
+}
+func (r *R) GetRole(ctx request.Ctx) {
+
+}
+func (r *R) UpdateRole(ctx request.Ctx) {
+
+}
+func (r *R) RemoveRole(ctx request.Ctx) {
+
+}
+func (r *R) AddUserRole(ctx request.Ctx) {
+
+}
+func (r *R) RemoveUserRole(ctx request.Ctx) {
+
+}
+func (r *R) ListAllPerm(ctx request.Ctx) {
+
+}
+func (r *R) ListAllRole(ctx request.Ctx) {
+
+}
+func (r *R) ListAllUserRole(ctx request.Ctx) {
+
+}
+func (r *R) ListAllUserPerm(ctx request.Ctx) {
+
+}
+func (r *R) ListUserPerm(ctx request.Ctx) {
+
+}
--- backend/server/app/routes/admin_user_group.go
+++ backend/server/app/routes/admin_user_group.go
@@ -1,6 +1,8 @@
 package routes
 
 import (
+	"strconv"
+
 	"github.com/temphia/core/backend/server/btypes/models/entities"
 	"github.com/temphia/core/backend/server/lib/apiutils/request"
 )
@@ -43,4 +45,255 @@ func (r *R) UpdateUserGroup(ctx request.Ctx) {
 
 func (r *R) RemoveUserGroup(ctx request.Ctx) {
 	r.WriteJSON(ctx.GinCtx, nil, r.cAdmin.RemoveUserGroup(ctx.Session, ctx.GinCtx.Param("user_group")))
+}
+
+// auth user group meta
+
+func (r *R) AddUserGroupAuth(ctx request.Ctx) {
+	data := &entities.UserGroupAuth{}
+	err := ctx.GinCtx.BindJSON(data)
+	if err != nil {
+		r.WriteErr(ctx.GinCtx, err.Error())
+		return
+	}
+
+	r.WriteJSON(
+		ctx.GinCtx,
+		nil,
+		r.cAdmin.AddUserGroupAuth(ctx.Session, ctx.GinCtx.Param("ugroup"), data),
+	)
+}
+
+func (r *R) UpdateUserGroupAuth(ctx request.Ctx) {
+	data := make(map[string]interface{})
+	err := ctx.GinCtx.BindJSON(data)
+	if err != nil {
+		r.WriteErr(ctx.GinCtx, err.Error())
+		return
+	}
+
+	id, err := strconv.ParseInt(ctx.GinCtx.Param("id"), 10, 64)
+	if err != nil {
+		r.WriteErr(ctx.GinCtx, err.Error())
+		return
+	}
+
+	r.WriteJSON(
+		ctx.GinCtx,
+		nil,
+		r.cAdmin.UpdateUserGroupAuth(ctx.Session, ctx.GinCtx.Param("ugroup"), id, data),
+	)
+}
+
+func (r *R) ListUserGroupAuth(ctx request.Ctx) {
+	data, err := r.cAdmin.ListUserGroupAuth(ctx.Session, ctx.GinCtx.Param("ugroup"))
+	r.WriteJSON(ctx.GinCtx, data, err)
+}
+
+func (r *R) GetUserGroupAuth(ctx request.Ctx) {
+
+	id, err := strconv.ParseInt(ctx.GinCtx.Param("id"), 10, 64)
+	if err != nil {
+		r.WriteErr(ctx.GinCtx, err.Error())
+		return
+	}
+
+	resp, err := r.cAdmin.GetUserGroupAuth(ctx.Session, ctx.GinCtx.Param("ugroup"), id)
+	r.WriteJSON(ctx.GinCtx, resp, err)
+}
+func (r *R) RemoveUserGroupAuth(ctx request.Ctx) {
+	id, err := strconv.ParseInt(ctx.GinCtx.Param("id"), 10, 64)
+	if err != nil {
+		r.WriteErr(ctx.GinCtx, err.Error())
+		return
+	}
+
+	err = r.cAdmin.RemoveUserGroupAuth(ctx.Session, ctx.GinCtx.Param("ugroup"), id)
+	r.WriteJSON(ctx.GinCtx, nil, err)
+}
+
+// hook
+
+func (r *R) AddUserGroupHook(ctx request.Ctx) {
+	data := &entities.UserGroupHook{}
+	err := ctx.GinCtx.BindJSON(data)
+	if err != nil {
+		r.WriteErr(ctx.GinCtx, err.Error())
+		return
+	}
+
+	r.WriteJSON(
+		ctx.GinCtx,
+		nil,
+		r.cAdmin.AddUserGroupHook(ctx.Session, ctx.GinCtx.Param("ugroup"), data),
+	)
+}
+
+func (r *R) UpdateUserGroupHook(ctx request.Ctx) {
+	data := make(map[string]interface{})
+	err := ctx.GinCtx.BindJSON(data)
+	if err != nil {
+		r.WriteErr(ctx.GinCtx, err.Error())
+		return
+	}
+
+	id, err := strconv.ParseInt(ctx.GinCtx.Param("id"), 10, 64)
+	if err != nil {
+		r.WriteErr(ctx.GinCtx, err.Error())
+		return
+	}
+
+	r.WriteJSON(
+		ctx.GinCtx,
+		nil,
+		r.cAdmin.UpdateUserGroupHook(ctx.Session, ctx.GinCtx.Param("ugroup"), id, data),
+	)
+}
+
+func (r *R) ListUserGroupHook(ctx request.Ctx) {
+	resp, err := r.cAdmin.ListUserGroupHook(ctx.Session, ctx.GinCtx.Param("ugroup"))
+	r.WriteJSON(ctx.GinCtx, resp, err)
+}
+
+func (r *R) GetUserGroupHook(ctx request.Ctx) {
+	id, err := strconv.ParseInt(ctx.GinCtx.Param("id"), 10, 64)
+	if err != nil {
+		r.WriteErr(ctx.GinCtx, err.Error())
+		return
+	}
+
+	resp, err := r.cAdmin.GetUserGroupHook(ctx.Session, ctx.GinCtx.Param("ugroup"), id)
+	r.WriteJSON(ctx.GinCtx, resp, err)
+}
+
+func (r *R) RemoveUserGroupHook(ctx request.Ctx) {
+	id, err := strconv.ParseInt(ctx.GinCtx.Param("id"), 10, 64)
+	if err != nil {
+		r.WriteErr(ctx.GinCtx, err.Error())
+		return
+	}
+
+	r.cAdmin.RemoveUserGroupHook(ctx.Session, ctx.GinCtx.Param("ugroup"), id)
+}
+
+// plug
+
+func (r *R) AddUserGroupPlug(ctx request.Ctx) {
+	data := &entities.UserGroupPlug{}
+	err := ctx.GinCtx.BindJSON(data)
+	if err != nil {
+		r.WriteErr(ctx.GinCtx, err.Error())
+		return
+	}
+
+	r.WriteJSON(
+		ctx.GinCtx,
+		nil,
+		r.cAdmin.AddUserGroupPlug(ctx.Session, ctx.GinCtx.Param("ugroup"), data),
+	)
+}
+
+func (r *R) UpdateUserGroupPlug(ctx request.Ctx) {
+	data := make(map[string]interface{})
+	err := ctx.GinCtx.BindJSON(data)
+	if err != nil {
+		r.WriteErr(ctx.GinCtx, err.Error())
+		return
+	}
+
+	id, err := strconv.ParseInt(ctx.GinCtx.Param("id"), 10, 64)
+	if err != nil {
+		r.WriteErr(ctx.GinCtx, err.Error())
+		return
+	}
+
+	err = r.cAdmin.UpdateUserGroupPlug(ctx.Session, ctx.GinCtx.Param("ugroup"), id, data)
+	r.WriteJSON(ctx.GinCtx, nil, err)
+}
+
+func (r *R) ListUserGroupPlug(ctx request.Ctx) {
+	resp, err := r.cAdmin.ListUserGroupPlug(ctx.Session, ctx.GinCtx.Param("ugroup"))
+	r.WriteJSON(ctx.GinCtx, resp, err)
+}
+
+func (r *R) GetUserGroupPlug(ctx request.Ctx) {
+	id, err := strconv.ParseInt(ctx.GinCtx.Param("id"), 10, 64)
+	if err != nil {
+		r.WriteErr(ctx.GinCtx, err.Error())
+		return
+	}
+
+	resp, err := r.cAdmin.GetUserGroupPlug(ctx.Session, ctx.GinCtx.Param("ugroup"), id)
+	r.WriteJSON(ctx.GinCtx, resp, err)
+}
+
+func (r *R) RemoveUserGroupPlug(ctx request.Ctx) {
+	id, err := strconv.ParseInt(ctx.GinCtx.Param("id"), 10, 64)
+	if err != nil {
+		r.WriteErr(ctx.GinCtx, err.Error())
+		return
+	}
+
+	r.cAdmin.RemoveUserGroupPlug(ctx.Session, ctx.GinCtx.Param("ugroup"), id)
+}
+
+// data
+
+func (r *R) AddUserGroupData(ctx request.Ctx) {
+	data := &entities.UserGroupData{}
+	err := ctx.GinCtx.BindJSON(data)
+	if err != nil {
+		r.WriteErr(ctx.GinCtx, err.Error())
+		return
+	}
+
+	r.WriteJSON(
+		ctx.GinCtx,
+		nil,
+		r.cAdmin.AddUserGroupData(ctx.Session, ctx.GinCtx.Param("ugroup"), data),
+	)
+}
+
+func (r *R) UpdateUserGroupData(ctx request.Ctx) {
+	data := make(map[string]interface{})
+	err := ctx.GinCtx.BindJSON(data)
+	if err != nil {
+		r.WriteErr(ctx.GinCtx, err.Error())
+		return
+	}
+
+	id, err := strconv.ParseInt(ctx.GinCtx.Param("id"), 10, 64)
+	if err != nil {
+		r.WriteErr(ctx.GinCtx, err.Error())
+		return
+	}
+
+	err = r.cAdmin.UpdateUserGroupData(ctx.Session, ctx.GinCtx.Param("ugroup"), id, data)
+	r.WriteJSON(ctx.GinCtx, nil, err)
+}
+
+func (r *R) ListUserGroupData(ctx request.Ctx) {
+	resp, err := r.cAdmin.ListUserGroupData(ctx.Session, ctx.GinCtx.Param("ugroup"))
+	r.WriteJSON(ctx.GinCtx, resp, err)
+}
+
+func (r *R) GetUserGroupData(ctx request.Ctx) {
+	id, err := strconv.ParseInt(ctx.GinCtx.Param("id"), 10, 64)
+	if err != nil {
+		r.WriteErr(ctx.GinCtx, err.Error())
+		return
+	}
+
+	resp, err := r.cAdmin.GetUserGroupData(ctx.Session, ctx.GinCtx.Param("ugroup"), id)
+	r.WriteJSON(ctx.GinCtx, resp, err)
+}
+
+func (r *R) RemoveUserGroupData(ctx request.Ctx) {
+	id, err := strconv.ParseInt(ctx.GinCtx.Param("id"), 10, 64)
+	if err != nil {
+		r.WriteErr(ctx.GinCtx, err.Error())
+		return
+	}
+
+	r.cAdmin.RemoveUserGroupData(ctx.Session, ctx.GinCtx.Param("ugroup"), id)
 }
