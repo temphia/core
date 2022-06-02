08342bca0820428d1a62da485226f48e40f6b609
bornjre <bornjre@gmail.com>
2022-06-03 03:48:32 +0545

    stash

--- frontend/src/entries/portal/playground/board/board.svelte
+++ frontend/src/entries/portal/playground/board/board.svelte
@@ -1,42 +1,6 @@
 <script>
   import DraggableCard from "./draggable_card.svelte";
-
-  var pos1 = 0,
-    pos2 = 0,
-    pos3 = 0,
-    pos4 = 0;
-
-  let elmnt;
-
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = elmnt.offsetTop - pos2 + "px";
-    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
-  }
-
-  function closeDragElement() {
-    // stop moving when mouse button is released:
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
+  import Gallery from "./gallery.svelte";
 </script>
 
 <div class="w-full h-full relative bg-blue-100">
@@ -122,6 +86,29 @@
           </div>
 
   </DraggableCard>
+
+  <DraggableCard>
+    <div class="w-96 overflow-auto" style="max-height: 40rem;">
+      <Gallery gap="10" maxColumnWidth="200">
+        <img src="https://via.placeholder.com/180x200/1" alt="">
+        <img src="https://via.placeholder.com/200x280/1" alt="">
+        <img src="https://via.placeholder.com/250x200/1" alt="">
+        <img src="https://via.placeholder.com/140x310/1" alt="">
+        <img src="https://via.placeholder.com/280x300/1" alt="">
+        <img src="https://via.placeholder.com/210x180/1" alt="">
+        <img src="https://via.placeholder.com/210x200/1" alt="">
+        <img src="https://via.placeholder.com/200x250/1" alt="">
+        <img src="https://via.placeholder.com/220x200/1" alt="">
+        <img src="https://via.placeholder.com/180x240/1" alt="">
+        <img src="https://via.placeholder.com/210x210/1" alt="">
+        <img src="https://via.placeholder.com/200x220/1" alt="">
+        <img src="https://via.placeholder.com/210x200/1" alt="">
+      </Gallery>
+    </div>
+  </DraggableCard>
+
+
+  
 
 
 </div>
